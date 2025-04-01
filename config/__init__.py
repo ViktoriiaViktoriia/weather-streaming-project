09a2a416a97db3b524d9f9feba251b806cbb81c1
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2025-04-01 20:03:06 +0300

    Update config/__init__.py

---
+++ config/__init__.py
@@ -0,0 +1,3 @@
+from .logger_config import logger
+from .config import (OPENWEATHER_API_KEY, KAFKA_BROKER, KAFKA_TOPIC, DB_NAME, DB_HOST, DB_PORT, DB_USER,
+                     DB_PASSWORD, GCS_BUCKET)
