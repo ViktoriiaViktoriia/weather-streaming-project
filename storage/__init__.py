fe8a113e9db4d1390cfc7a04b5f33db4e7620f52
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2025-04-02 10:41:24 +0300

    Update database_setup.py, add create connection function, close connection function, execute query function

---
+++ storage/__init__.py
@@ -0,0 +1,1 @@
+from database_setup import create_connection, close_connection, execute_query
---
+++ storage/database_setup.py
@@ -0,0 +1,45 @@
+import psycopg2
+
+from config import DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, logger
+
+
+def create_connection():
+    """Create and return a PostgreSQL database connection."""
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+        logger.info(f"Successfully connected to database: {DB_NAME} on {DB_HOST}:{DB_PORT}")
+        return conn
+    except Exception as e:
+        logger.info(f"Error: Unable to connect to the database. {e}")
+        return None
+
+
+def close_connection(conn):
+    """Close the database connection."""
+    if conn:
+        conn.close()
+        logger.info("Connection closed.")
+
+
+def execute_query(query, params=None):
+    """Execute a SQL query."""
+    conn = create_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+            result = cursor.fetchall()
+            cursor.close()
+            logger.info("The query executed successfully.")
+            return result
+        except Exception as e:
+            logger.info(f"Error executing query: {e}")
+        finally:
+            close_connection(conn)
+    return None
