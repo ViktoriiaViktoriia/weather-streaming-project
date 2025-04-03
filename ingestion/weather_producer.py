6132999d3146a1c9e5a3af6a09be209845c14363
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2025-04-03 21:56:16 +0300

    Updated stream_weather_data function to include automatic retries with exponential backoff.

--- ingestion/weather_producer.py
+++ ingestion/weather_producer.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from confluent_kafka import Producer
+import time
 
 from config import logger, API_KEY, BASE_URL, KAFKA_TOPIC, KAFKA_BROKER
 
@@ -46,12 +47,36 @@ def send_message_to_kafka(topic, data):
 
 
 def stream_weather_data():
-    """Fetches weather data periodically and sends to Kafka."""
-    while True:
-        for city in CITIES:
-            weather_data = fetch_weather(city)
-            if weather_data:
-                # Stream to Kafka
-                send_message_to_kafka(KAFKA_TOPIC, weather_data)
-                logger.info(f"Sent weather data for {city} to Kafka.")
+    """Periodically fetches weather data with automatic retries and exponential backoff,
+    then streams it to Kafka."""
+
+    for city in CITIES:
+        retries = 0      # Reset retry counter for each city
+        max_retries = 5
+        backoff_time = 300   # Initial backoff time in seconds (5 minutes)
+
+        while retries < max_retries:  # Retry logic applies to the current city
+            try:
+                weather_data = fetch_weather(city)
+                logger.info(f"Successfully fetched data for {city}")
+
+                if weather_data:
+                    send_message_to_kafka(KAFKA_TOPIC, weather_data)
+                    logger.info(f"Sent weather data for {city} to Kafka.")
+                break  # Exit retry loop for the current city if successful
+
+            except Exception as e:
+                logger.warning(f"Failed to fetch data for {city}: {e}. Retrying...")
+                retries += 1
+
+                if retries < max_retries:
+                    logger.info(f"Retrying in {backoff_time / 60:.2f} minutes...")
+                    time.sleep(backoff_time)
+                    backoff_time *= 2  # Doubles backoff time for the current city
+                else:
+                    logger.error(f"Max retries reached for {city}. Skipping.")
+
+
+
+
 
