2bcaf9ac9bae011a3f1bac0b6c0573296ce6d91e
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2025-04-04 11:38:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-import time
-
 from config import logger
 from ingestion import stream_weather_data
 
 
 def main():
     try:
-        # Main loop to periodically fetch and stream weather data
-        while True:
-            # Handles the periodic fetching and sending of weather data
-            stream_weather_data()
-            logger.info(f"Weather data for streamed successfully.")
-
-            # Sleep for 5 minutes (300 seconds) before the next fetch
-            logger.info(f"Waiting for 5 minutes before fetching data again...")
-            time.sleep(300)
+        # Start weather data streaming
+        stream_weather_data()
+        logger.info("Weather data streamed successfully.")
     except KeyboardInterrupt:
         logger.info("Weather data streaming interrupted.")
     except Exception as e:
-        logger.error(f"An error occurred: {e}")
+        logger.error(f"An unexpected error occurred: {e}")
 
 
 if __name__ == "__main__":
