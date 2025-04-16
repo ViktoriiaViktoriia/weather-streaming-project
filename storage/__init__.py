86c97dcc55408bdf44e8052cbae38e98ab234cc7
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2025-04-16 11:59:53 +0300

    Update  storage/__init__.py

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,2 +1,2 @@
-from database_setup import create_connection, close_connection, execute_query
-from upload_to_gcs import upload_to_gcs
+#from .database_setup import create_connection, close_connection, execute_query
+from .upload_to_gcs import upload_to_gcs
