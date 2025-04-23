77142280472ef7a5667f33765808560db0f63f92
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2025-04-24 01:34:16 +0300

    Update function list

--- processing/__init__.py
+++ processing/__init__.py
@@ -1,2 +1,2 @@
 from .weather_consumer import weather_data_consumer, write_row
-from .weather_processing import load_all_csvs
+from .weather_processing import process_weather_data, insert_weather_data, load_all_csvs
